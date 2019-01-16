d297a1a611c777c93ef53b159ad9922a58f68835
Justin Clark <justin.d.clark@protonmail.com>
2019-01-15 23:03:20 -0500

    Solution submitted

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,20 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+    // Create an empty array to hold string
+    let stringArray = [];
+
+    // loop through and copy string to array
+    for (let i = 0; i < someStr.length; i++) {
+        stringArray.push(someStr.charAt(i));
+    }
+
+    // JavaScript strings don't have a built-in 
+    // reverse() method, but arrays do!
+    return stringArray.reverse.toString;
 }
+
+let myString = "This assignment was too easy";
+
+console.log(`Forward: "${myString}"`);
+console.log(`Reverse: ${reverseStr(myString)}`);
