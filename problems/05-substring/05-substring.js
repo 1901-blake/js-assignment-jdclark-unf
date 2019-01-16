606899ee36da334f0a919ce111c63b4bbf19987e
Justin Clark <justin.d.clark@protonmail.com>
2019-01-16 16:31:56 -0500

    Suolution submitted

--- problems/05-substring/05-substring.js
+++ problems/05-substring/05-substring.js
@@ -4,5 +4,15 @@ Return the substring contained between startIndex (inclusive) and endIndex (excl
 If incorrect input is entered, throw an exception stating the input was incorrect.
 For example substring('hello', 0, 3) should give 'hel' */
 function substring(someStr, startIndex, endIndex) {
-
+    try {
+        return `${someStr.slice(startIndex, endIndex)}`;
+    }
+    catch(e) {
+        return 'Invalid input';
+    }
 }
+// good input
+console.log(substring('Supercalifragilisticexpialidocious', 3, 15));
+
+// bad input
+console.log(substring(5, 0, 1));
