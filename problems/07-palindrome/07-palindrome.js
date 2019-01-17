0b134642ed7898027df741aa8307c1568ae32ee6
Justin Clark <justin.d.clark@protonmail.com>
2019-01-17 02:31:35 -0500

    Solution submitted

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,24 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+    // Create an empty array to hold string
+    let stringArray = [];
+
+    // loop through and copy string to array
+    for (let i = 0; i < someStr.length; i++) {
+        stringArray.push(someStr.charAt(i));
+    }
+
+    // If array is the same forwards and backwards, string is a palindrome
+    if (stringArray.join() === stringArray.reverse().join()) {
+        return `${someStr} is a palindrome`
+    } else {
+        return `${someStr} is not a palindrome`
+    }
 }
+
+console.log(isPalindrome('ABBA'));
+console.log(isPalindrome('AVID DIVA'));
+console.log(isPalindrome('RACECAR'));
+console.log(isPalindrome('PALINDROME'));
+console.log(isPalindrome('MADAME'));
