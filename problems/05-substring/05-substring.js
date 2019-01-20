b4754ca0302b30de81dff366fcbdec11a4b5b44a
Justin Clark <justin.d.clark@protonmail.com>
2019-01-19 21:32:54 -0500

    Solution submitted

--- problems/05-substring/05-substring.js
+++ problems/05-substring/05-substring.js
@@ -5,14 +5,36 @@ If incorrect input is entered, throw an exception stating the input was incorrec
 For example substring('hello', 0, 3) should give 'hel' */
 function substring(someStr, startIndex, endIndex) {
     try {
-        return `${someStr.slice(startIndex, endIndex)}`;
+        if (typeof(someStr) !== 'string') {
+            throw new TypeError('First parameter must be a string');
+        } else if (typeof(startIndex) !== 'number') {
+            throw new TypeError('Second parameter must be a number');
+        } else if (typeof(endIndex) !== 'number') {
+            throw new TypeError('Third parameter must be a number');
+        } else {
+            return `${someStr.slice(startIndex, endIndex)}`;
+        }        
     }
     catch(e) {
-        return 'Invalid input';
+        return `${e.name}: ${e.message}`;      
     }
 }
 // good input
-console.log(substring('Supercalifragilisticexpialidocious', 3, 15));
+console.log(`\nTest 1: string = 'Supercalifragilisticexpialidocious', start = 0, end = 15
+Result: ${substring('Supercalifragilisticexpialidocious', 0, 15)}`);
 
-// bad input
-console.log(substring(5, 0, 1));
+// good input
+console.log(`\nTest 2: string = 'Larry, Moe, Curly', start = 7, end = 10
+Result: ${substring('Larry, Moe, Curly', 7, 10)}`);
+
+// bad input (first parameter not a string)
+console.log(`\nTest 3: string = 4, start = 0, end = 2
+Result: ${substring(4, 0, 2)}`);
+
+// bad input (second param not a number)
+console.log(`\nTest 4: string = 'test', start = null, end = 3
+Result: ${substring('test', null, 3)}`);
+
+// bad input (third paraneter not a number)
+console.log(`\nTest 5: string = 'foobar', start = 3, end = 'end'
+Result: ${substring('foobar', 3, 'end')}`);
